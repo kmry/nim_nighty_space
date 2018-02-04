d0bbbbef3aaffaa96f8c9a00f9f09871b218fa4b
kmry <kmr_rtf@live.jp>
2018-02-04 13:50:09 +0900

    Update make.profile.sh

--- init/make.profile.sh
+++ init/make.profile.sh
@@ -3,5 +3,6 @@ nim_path=`pwd`/Nim/bin
 
 ## only if you use bash 
 profile=~/.bash_profile
-echo "export PATH=$PATH:${nim_path}" >> ${profile}
+echo 'export PATH=$PATH':${nim_path} >> ${profile}
 source ${profile}
+
