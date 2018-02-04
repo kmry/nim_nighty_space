7fafee01c5573868f193bb52b78a2da8516eb688
kmry1 <kimura@miyavi.asia>
2018-02-04 04:25:48 +0000

    rev.

--- init/nim_nighty.sh
+++ init/nim_nighty.sh
@@ -2,3 +2,11 @@ git clone https://github.com/nim-lang/Nim.git
 cd Nim
 git clone --depth 1 https://github.com/nim-lang/csources.git
 cd csources
+sh build.sh
+cd ../
+bin/nim c koch
+./koch boot -d:release
+
+##to build toools incl.nimble.
+./koch tools
+
